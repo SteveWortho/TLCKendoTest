b8de097b5744f01e2a11e79e8cbbfbad1a8901d6
SteveWortho <steve@jungle.co.nz>
2018-01-11 11:26:02 +1300

    Add project files.

---
+++ TLCKendo/Models/SearchModel.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace TLCKendo.Models
+{
+    public class SearchModel
+    {
+        public int CourseId { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
---
+++ TLCKendo/Models/SimpleLookupItem.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TLCKendo.Models
+{
+    public class SimpleLookupItem
+    {
+        public int CategoryId { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
